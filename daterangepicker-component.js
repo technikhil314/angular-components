dc31b030787901c58e3753ddd7eedd684dfd7408
Nikhil Mehta <nikhil.001mehta@gmail.com>
2017-01-17 18:26:25 +0530

    adding options for min and max dates. Also using moment object throughout internally. Adding pipe for moment format

--- daterangepicker-component.js
+++ daterangepicker-component.js
@@ -8,17 +8,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
-var daterangepicker_options_1 = require("./daterangepicker-options");
-var moment = require("moment");
+var core_1 = require('@angular/core');
+var daterangepicker_options_1 = require('./daterangepicker-options');
+var moment = require('moment');
 var DaterangepickerComponent = (function () {
     function DaterangepickerComponent(elem) {
         this.elem = elem;
+        //outputs
         this.rangeSelected = new core_1.EventEmitter();
         this.range = "";
-        this.toMonth = moment([this.toYear, this.toMonth]).add(1, 'months').get('month');
-        this.fromMonth = moment([this.fromYear, this.fromMonth]).get('month');
     }
+    //handle outside/inside click to show rangepicker
     DaterangepickerComponent.prototype.handleOutsideClick = function (event) {
         var current = event.target;
         var host = this.elem.nativeElement;
@@ -44,18 +44,31 @@ var DaterangepickerComponent = (function () {
             } while (current);
         }
         this.showCalendars = false;
+        this.updateCalendar();
     };
-    DaterangepickerComponent.prototype.ngOnInit = function () {
-        var tDate = moment(this.options.startDate, this.options.format);
+    DaterangepickerComponent.prototype.updateCalendar = function () {
+        //get month and year to show calendar
+        var tDate = moment(this.fromDate, this.format);
         this.fromMonth = tDate.get('month');
         this.fromYear = tDate.get('year');
-        tDate = moment(this.options.startDate, this.options.format).add(1, 'months');
+        tDate = moment(this.fromDate, this.format).add(1, 'months');
         this.toMonth = tDate.get('month');
         this.toYear = tDate.get('year');
-        this.fromDate = this.options.startDate;
-        this.toDate = this.options.endDate;
-        this.format = this.options.format;
-        this.range = this.options.startDate + " - " + this.options.endDate;
+        this.setRange();
+    };
+    DaterangepickerComponent.prototype.ngOnInit = function () {
+        //get default options provided by user
+        if (this.options) {
+            this.format = this.options.format || "YYYY-MM-DD";
+            this.fromDate = this.getMoment(this.options.startDate) || moment();
+            this.toDate = this.getMoment(this.options.endDate) || moment();
+        }
+        else {
+            this.format = "YYYY-MM-DD";
+            this.fromDate = moment();
+            this.toDate = moment();
+        }
+        this.updateCalendar();
     };
     DaterangepickerComponent.prototype.dateChanged = function (value) {
         if ((this.fromDate && this.toDate) || !(this.fromDate || this.toDate)) {
@@ -65,47 +78,65 @@ var DaterangepickerComponent = (function () {
         else if (this.fromDate && !this.toDate) {
             this.toDate = value;
             this.showCalendars = false;
-            this.range = this.fromDate + " - " + this.toDate;
-            this.rangeSelected.emit(this.range);
+            this.setRange();
+            this.emitRangeSelected();
         }
+    };
+    DaterangepickerComponent.prototype.emitRangeSelected = function () {
+        this.rangeSelected.emit({
+            start: this.getMoment(this.fromDate),
+            end: this.getMoment(this.toDate)
+        });
     };
     DaterangepickerComponent.prototype.monthChanged = function (value) {
         var temp;
         temp = moment([this.fromYear, this.fromMonth]).add(value, 'months');
-        this.fromMonth = temp.get('month');
         this.fromMonth = temp.get('month');
         this.fromYear = temp.get('year');
         temp = moment([this.toYear, this.toMonth]).add(value, 'months');
         this.toMonth = temp.get('month');
         this.toYear = temp.get('year');
     };
+    DaterangepickerComponent.prototype.getMoment = function (value) {
+        return moment(value, this.format);
+    };
+    DaterangepickerComponent.prototype.getValidaMoment = function (value) {
+        var momentValue = null;
+        if (moment(value, this.format, true).isValid()) {
+            momentValue = moment(value, this.format, true);
+        }
+        return momentValue;
+    };
+    DaterangepickerComponent.prototype.setRange = function () {
+        this.range = this.fromDate.format(this.format) + " - " + this.toDate.format(this.format);
+    };
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', daterangepicker_options_1.Options)
+    ], DaterangepickerComponent.prototype, "options", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', String)
+    ], DaterangepickerComponent.prototype, "className", void 0);
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], DaterangepickerComponent.prototype, "rangeSelected", void 0);
+    __decorate([
+        core_1.HostListener('document:mousedown', ['$event']),
+        core_1.HostListener('document:mouseup', ['$event']), 
+        __metadata('design:type', Function), 
+        __metadata('design:paramtypes', [Object]), 
+        __metadata('design:returntype', void 0)
+    ], DaterangepickerComponent.prototype, "handleOutsideClick", null);
+    DaterangepickerComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'date-range-picker',
+            templateUrl: './daterangepicker-component.html'
+        }), 
+        __metadata('design:paramtypes', [core_1.ElementRef])
+    ], DaterangepickerComponent);
     return DaterangepickerComponent;
 }());
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", daterangepicker_options_1.Options)
-], DaterangepickerComponent.prototype, "options", void 0);
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", String)
-], DaterangepickerComponent.prototype, "class", void 0);
-__decorate([
-    core_1.Output(),
-    __metadata("design:type", Object)
-], DaterangepickerComponent.prototype, "rangeSelected", void 0);
-__decorate([
-    core_1.HostListener('document:mousedown', ['$event']),
-    core_1.HostListener('document:mouseup', ['$event']),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Object]),
-    __metadata("design:returntype", void 0)
-], DaterangepickerComponent.prototype, "handleOutsideClick", null);
-DaterangepickerComponent = __decorate([
-    core_1.Component({
-        moduleId: module.id,
-        selector: 'date-range-picker',
-        templateUrl: './daterangepicker-component.html'
-    }),
-    __metadata("design:paramtypes", [core_1.ElementRef])
-], DaterangepickerComponent);
 exports.DaterangepickerComponent = DaterangepickerComponent;
---
+++ format-moment-date-pipe.ts
@@ -0,0 +1,8 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({name: 'formatMomentDate'})
+export class FormatMomentDatePipe implements PipeTransform {
+  transform(value: any, format: string): string {
+    if( value ) return value.format(format);
+	return null;
+  }
+}
